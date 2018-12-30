83e0d0cd2bee554ce0afd6e6dd67267642534b9b
renyan001 <370778337@qq.com>
2018-12-30 10:25:55 +0800

    修复bug

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -81,18 +81,17 @@
       </li>
     </ul>
   </div>
-  
 </template>
 
 <script>
 export default {
-  name: "HelloWorld",
-  data() {
+  name: 'HelloWorld',
+  data () {
     return {
-      msg: "Welcome to Your Vue.js App"
-    };
+      msg: 'Welcome to Your Vue.js App'
+    }
   }
-};
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
