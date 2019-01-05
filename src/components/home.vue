37d423d43ee5dde922e95bacfb03b6fc788d1ecb
renyan001 <370778337@qq.com>
2019-01-05 17:59:07 +0800

    动态导航+导航守卫

--- src/components/home.vue
+++ src/components/home.vue
@@ -26,72 +26,16 @@
         :router="true"
         >
         <!-- 1 -->
-        <el-submenu index="1">
+        <el-submenu :index="''+item1.order" v-for="(item1,i) in menus" :key="item1.id">
           <template slot="title">
             <i class="el-icon-location"></i>
-            <span>用户管理</span>
+            <span>{{item1.authName}}</span>
           </template>
-            <el-menu-item index="users">
+            <el-menu-item :index="item2.path" v-for="(item2,i) in item1.children" :key="item2.id">
               <i class="el-icon-menu"></i>
-              用户列表
+              {{item2.authName}}
             </el-menu-item>
-        </el-submenu>
-        <!-- 2 -->
-        <el-submenu index="2">
-          <template slot="title">
-            <i class="el-icon-location"></i>
-            <span>权限管理</span>
-          </template>
-            <el-menu-item index="roles">
-              <i class="el-icon-menu"></i>
-              角色列表
-            </el-menu-item>
-            <el-menu-item index="rights">
-              <i class="el-icon-menu"></i>
-              权限列表
-            </el-menu-item>
-        </el-submenu>
-        <!-- 3 -->
-        <el-submenu index="3">
-          <template slot="title">
-            <i class="el-icon-location"></i>
-            <span>商品管理</span>
-          </template>
-            <el-menu-item index="3-1">
-              <i class="el-icon-menu"></i>
-              商品列表
-            </el-menu-item>
-            <el-menu-item index="3-2">
-              <i class="el-icon-menu"></i>
-              分类参数
-            </el-menu-item>
-            <el-menu-item index="3-3">
-              <i class="el-icon-menu"></i>
-              商品分类
-            </el-menu-item>
-        </el-submenu>
-        <!-- 4 -->
-        <el-submenu index="4">
-          <template slot="title">
-            <i class="el-icon-location"></i>
-            <span>订单管理</span>
-          </template>
-            <el-menu-item index="4-1">
-              <i class="el-icon-menu"></i>
-              订单列表
-            </el-menu-item>
-        </el-submenu>
-        <!-- 5 -->
-        <el-submenu index="5">
-          <template slot="title">
-            <i class="el-icon-location"></i>
-            <span>数据统计</span>
-          </template>
-            <el-menu-item index="5-1">
-              <i class="el-icon-menu"></i>
-              数据报表
-            </el-menu-item>
-        </el-submenu>
+        </el-submenu>        
       </el-menu>
       </el-aside>
       <el-main class="main">
@@ -107,16 +51,28 @@
 export default {
   // 在组建渲染之前
   beforeCreate () {
-    if (!localStorage.getItem('token')) {
-      // 回到登录
-      this.$router.push({
-        name: 'login'
-      })
-      // 提示
-      this.$message.warning('请先登录')
-    }
+    // if (!localStorage.getItem('token')) {
+    //   // 回到登录
+    //   this.$router.push({
+    //     name: 'login'
+    //   })
+    //   // 提示
+    //   this.$message.warning('请先登录')
+    // }
+  },
+  created() {
+    this.getMenus();
+  },
+  data() {
+    return {
+      menus:[]
+    };    
   },
   methods: {
+    async getMenus() {
+      const res = await this.$http.get(`menus`);
+      this.menus = res.data.data;
+    },
     handleLoginout () {
       localStorage.clear()
       this.$router.push({
--- src/router/index.js
+++ src/router/index.js
@@ -5,10 +5,16 @@ import Home from '@/components/home.vue'
 import Users from '@/components/users.vue'
 import Rights from '@/components/rights.vue'
 import Roles from '@/components/roles.vue'
+import Goodslist from '@/components/roles.vue'
+import GoodsAdd from '@/components/roles.vue'
+// 单独引入组件
+import {
+  Message
+} from 'element-ui'
 
 Vue.use(Router)
 
-export default new Router({
+const router =  new Router({
   routes: [
     {
       path: '/',
@@ -29,6 +35,16 @@ export default new Router({
           name: 'roles',
           path: '/roles',
           component: Roles
+        },
+        {
+          name: 'goods',
+          path: '/goods',
+          component: Goodslist
+        },
+        {
+          name: 'goodsadd',
+          path: '/goods/add',
+          component: GoodsAdd
         }
       ]
     },
@@ -39,3 +55,26 @@ export default new Router({
     }
   ]
 })
+
+// 拦截路由配置
+router.beforeEach((to, from, next) => {
+  // 如果要去的是login 继续执行路由配置
+  if (to.name === 'login') {
+    next();
+  } else {
+    // 如果要去home 判断token 如果有token ,next()  如果没有token 返回到login
+    const token = localStorage.getItem('token');
+    if (!token) {
+      Message.warning("请先登录");
+      router.push({
+        name: 'login'
+      });
+    } else {
+      next();
+    }
+  }
+
+  next(); //前往下一个页面
+  })
+
+export default router;
