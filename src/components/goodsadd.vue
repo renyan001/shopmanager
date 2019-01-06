c12a50c25929d488b695b376a714c1d46b137749
renyan001 <370778337@qq.com>
2019-01-06 10:30:14 +0800

    goodsadd开始

--- src/components/goodsadd.vue
+++ src/components/goodsadd.vue
@@ -16,14 +16,34 @@
       <el-step title="商品图片"></el-step>
       <el-step title="商品内容"></el-step>
     </el-steps>
+    
+    <el-form label-position="top" model="form" label-width="80px" class="elform">
+       <el-tabs tab-position="left" v-model="active">
+        <el-tab-pane name="1" label="基本信息">
+          <el-form-item label="商品名称">
+            <el-input></el-input>
+          </el-form-item>
+          <el-form-item label="商品价格">
+            <el-input></el-input>
+          </el-form-item>
+          <el-form-item label="商品重量">
+            <el-input></el-input>
+          </el-form-item>
+          <el-form-item label="商品数量">
+            <el-input></el-input>
+          </el-form-item>
+          <el-form-item label="商品分类">
+            <el-input></el-input>
+          </el-form-item>
+        </el-tab-pane>
+        <el-tab-pane name="2" label="商品参数">商品参数</el-tab-pane>
+        <el-tab-pane name="3" label="商品属性">商品属性</el-tab-pane>
+        <el-tab-pane name="4" label="商品图片">商品图片</el-tab-pane>
+        <el-tab-pane name="5" label="商品内容">商品内容</el-tab-pane>
+      </el-tabs>   
+    </el-form>
     <!-- 标签页el-tab -->
-    <el-tabs tab-position="left" v-model="active">
-      <el-tab-pane name="1" label="基本信息">基本信息</el-tab-pane>
-      <el-tab-pane name="2" label="商品参数">商品参数</el-tab-pane>
-      <el-tab-pane name="3" label="商品属性">商品属性</el-tab-pane>
-      <el-tab-pane name="4" label="商品图片">商品图片</el-tab-pane>
-      <el-tab-pane name="5" label="商品内容">商品内容</el-tab-pane>
-    </el-tabs>
+    
   </el-card>
 </template>
 
@@ -47,5 +67,9 @@ export default {
   }
   .step {
     margin-bottom: 20px;
+  }
+  .elform {
+    height: 400px;
+    overflow: hidden;
   }
 </style>
